4fe04e3468efd2393df409e4a0c95b51a10d261c
Buld0z3r <127163911+Buld0z3r@users.noreply.github.com>
2024-11-11 18:16:58 +0100

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,4 @@
-Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> import unittest
+import unittest
 ... from calculator import Calculator
 ... 
 ... class TestCalculator(unittest.TestCase):
